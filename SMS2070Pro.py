5979c2d68ce865010ccb00ce2cb19b2b509e4f5d
Leonid <80206743+Leon20082008@users.noreply.github.com>
2021-08-10 20:08:23 +0600

    Изменил управление

--- SMS2070Pro.py
+++ SMS2070Pro.py
@@ -87,9 +87,9 @@ class Player(GameSprite):
     def update(self):
         global is_busy
         keys = key.get_pressed()
-        if keys[K_LEFT] and self.rect.x > 5:
+        if keys[K_a] and self.rect.x > 5:
             self.rect.x -= self.speed
-        if keys[K_RIGHT] and self.rect.x < win_width - 80:
+        if keys[K_d] and self.rect.x < win_width - 80:
             self.rect.x += self.speed
     # метод "выстрел" (используем место игрока, чтобы создать там пулю)
     def fire(self):
